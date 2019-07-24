dbc4644baad297023d13bc0f44b8becdca1f8eda
aswath1711 <40084282+aswath1711@users.noreply.github.com>
2019-07-24 21:56:00 +0530

    Add files via upload

---
+++ Hset3_2.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 24 21:53:48 2019
+
+@author: New
+"""
+
+n=int(input())
+l=list(map(int,input().split()))
+r=1
+l1=[]
+for i in l:
+  r=r*i
+for i in l:
+  l1.append(remd//i)
+print(*1l)
